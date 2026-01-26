b441c8fcb39a3d635fb4404137f2d3afd32b5ba0
feed0 <dogmas-gutters.07@icloud.com>
2026-01-26 07:59:07 -0300

    [SK-02] Create an app with a list of players
    1. Added scores list
    2. Refactored viewbuilders
    3. Added a stepper for each score for user interaction

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -10,18 +10,32 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var players: [String] = ["Andre", "Bruna", "Carla"]
+    @State private var scores: [Int] = [0, 0, 0]
     
     var body: some View {
         VStack {
-            ForEach(0..<players.count, id: \.description) { index in
-                TextField("Name", text: $players[index])
-            }
-            
-            Button("Add player", systemImage: "plus") {
-                players.append("")
-            }
+            playersList
+            Spacer()
+            addPlayerButton
         }
         .padding()
+    }
+    
+    // MARK: - ViewBuilder
+    
+    private var playersList: some View {
+        ForEach(0..<players.count, id: \.description) { index in
+            
+            TextField("Name", text: $players[index])
+            Stepper("\(scores[index])", value: $scores[index])
+        }
+    }
+    
+    private var addPlayerButton: some View {
+        Button("Add player", systemImage: "plus") {
+            players.append("")
+            scores.append(0)
+        }
     }
 }
 
